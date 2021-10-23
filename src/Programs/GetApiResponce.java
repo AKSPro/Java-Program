5b3831445e3ca9c52f85edb44ac08b99817658be
AKSPro <sonar.akhilesh@gmail.com>
2021-10-24 01:13:26 +0530

    test

--- src/Programs/GetApiResponce.java
+++ src/Programs/GetApiResponce.java
@@ -4,14 +4,6 @@ public class GetApiResponce {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
-//from master
-int a;
-///from new
-
-//from new
-=======
->>>>>>> aadc643b62392afd765f935676621c1dfaaf3241
 	}
 
 }
