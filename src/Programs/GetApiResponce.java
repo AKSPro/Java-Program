aadc643b62392afd765f935676621c1dfaaf3241
AKSPro <sonar.akhilesh@gmail.com>
2021-10-24 01:05:21 +0530

    from master

--- src/Programs/GetApiResponce.java
+++ src/Programs/GetApiResponce.java
@@ -3,14 +3,7 @@ package Programs;
 public class GetApiResponce {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-///aks
 
-//from master
-int a;
-
-//from new
 	}
 
 }
